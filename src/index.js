fa1623103f3bcfac64a2f43aaae7303fe8567228
Zaiko Euheni <rahunak@yandex.by>
2022-05-19 12:23:23 +0300

    feat: did header and markup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'normalize.css';
+import './components/style.css';
+import './components/scss/fonts.scss';
+import './components/scss/main.scss';
+import './components/scss/header.scss';
+import './components/scss/slider.scss';
+import './components/scss/arcitect.scss';
+import './components/scss/cooperation.scss';
+import './components/scss/footer.scss';
