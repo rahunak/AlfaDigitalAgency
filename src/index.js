f3796b89d9cabd4e9152c25125a73b21624d9f50
Zaiko Euheni <rahunak@yandex.by>
2022-05-26 12:12:30 +0300

    feat: start adaptive

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import './components/scss/slider.scss';
 import './components/scss/architect.scss';
 import './components/scss/cooperation.scss';
 import './components/scss/footer.scss';
-
+// styles For popap
 import './components/scss/popap/popapMakeOrder.scss';
 import './components/scss/popap/popapMakeConsultation.scss';
 import './components/scss/popap/popapThanks.scss';
 import './components/scss/popap/popapWantToWatch.scss';
-
+// scripts
 import './components/scripts/eventListeners';
-
 import './components/scripts/showPopap';
